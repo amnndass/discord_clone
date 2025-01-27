9673c3e2179172711533696522523b3b43723d62
amnndass <ak7184103@gmail.com>
2025-01-27 16:12:45 +0530

    we back

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 
 export const currentProfile = async () => {
-    const {userId, redirectToSignIn}= auth();
+    const {userId, redirectToSignIn}= await auth();
 
     if(!userId) {
         return null;
@@ -13,4 +13,6 @@ export const currentProfile = async () => {
             userId
         }
     })
+
+    return profile;
 }
